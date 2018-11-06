fc62668d536e6ac63b21d96d7560de868c6fdea2
Ben Gyori <ben.gyori@gmail.com>
2018-11-06 16:06:18 -0500

    Add comments on docker base folder

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,7 +1,15 @@
-#/bin/sh
+#/bin/bash
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
 #NOTE: one actually has to log out and in again for this to take effect
+# Optionally, one can have ddocker make images in a custom location
+# To do this, /lib/systemd/system/docker.service, and and add the -g option
+# to this line:
+# ExecStart=/usr/bin/dockerd -g /pmc/docker -H fd://
+# then do
+# sudo systemctl daemon-reload
+# sudo service docker restart
+
